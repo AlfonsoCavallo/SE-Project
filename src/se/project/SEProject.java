2c46947c246da8cd4e108867d98da2bfa4e03133
delso <delso@192.168.1.9>
2020-11-20 16:08:47 +0100

    Andrea initial commit_3

--- src/se/project/SEProject.java
+++ src/se/project/SEProject.java
@@ -19,7 +19,7 @@ public class SEProject {
         // TODO code application logic here
         System.out.println("Hello World");
         System.out.println("Hello Giorgio");
-        System.out.println("Hello Andrea_2");
+        System.out.println("Hello Andrea_3");
     }
     
 }
