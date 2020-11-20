e41946299ddf9fe5447b9fca50d1786a20496651
Giorgio <Giorgio@DESKTOP-6S1K87J>
2020-11-20 12:43:40 +0100

    test repository

--- src/se/project/SEProject.java
+++ src/se/project/SEProject.java
@@ -18,6 +18,7 @@ public class SEProject {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.println("Hello Giorgio");
     }
     
 }
