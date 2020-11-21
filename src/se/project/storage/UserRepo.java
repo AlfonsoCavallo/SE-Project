d0a1f0c42c08516824da8f3c1644a3d7d798602d
AlfonsoCavallo <alfonso.cavallo98@gmail.com>
2020-11-21 15:55:44 +0100

    Defined interfaces for the Login Functionality

---
+++ src/se/project/storage/UserRepo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package se.project.storage;
+
+/**
+ *
+ * @author Utente
+ */
+public class UserRepo
+{
+    private String DATABASE_SERVER_ADDRESS;
+    
+    public UserRepo(String username, char[] password)
+    {
+        
+    }
+    
+    public User queryCurrentUser()
+    {
+        // Return a model of the current user
+        return null;
+    }
+    
+}
