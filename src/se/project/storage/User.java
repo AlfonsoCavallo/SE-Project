3589fa85b119a88b9be6f81e2003ab73d4677219
AlfonsoCavallo <alfonso.cavallo98@gmail.com>
2020-11-21 16:02:47 +0100

    Defined interfaces for the Login Functionality 2

---
+++ src/se/project/storage/User.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package se.project.storage;
+
+/**
+ *
+ * @author Utente
+ */
+public class User 
+{
+    enum Role
+    {
+        SYSTEM_ADMINISTRATOR,
+        PLANNER
+    }
+    
+    public User(Role role, String username, char[] password)
+    {
+    }
+    
+    public Role getRole()
+    {
+        // Returns the role of the User
+        return null;
+    }
+    
+    public String getUsername()
+    {
+        // Returns username
+        return null;
+    }
+    
+    public char[] getPassword()
+    {
+        // Returns password
+        return null;
+    }
+}
