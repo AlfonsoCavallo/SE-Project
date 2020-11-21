28604f2294c52e4a18f93ce04e3c11280f5fba92
AlfonsoCavallo <alfonso.cavallo98@gmail.com>
2020-11-21 16:04:05 +0100

    Defined interfaces for the Login Functionality 34

---
+++ src/se/project/business_logic/controllers/LoginController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package se.project.business_logic.controllers;
+
+/**
+ *
+ * @author Utente
+ */
+public class LoginController
+{
+
+    public void login() 
+    {
+        
+    }
+    
+}
