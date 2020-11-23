75ef1fd56b7607e6074e2ef3f73e1e441733a7a0
Andrea <delso@192.168.1.9>
2020-11-23 11:33:38 +0100

    Implementation of UserRepo class

--- src/se/project/storage/UserRepo.java
+++ src/se/project/storage/UserRepo.java
@@ -6,32 +6,68 @@
 
 package se.project.storage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import se.project.storage.User.Role;
+
 /**
  *
  * @author Utente
  */
 public class UserRepo
 {
-    private String DATABASE_SERVER_ADDRESS;
+    private Connection connection = null;
+    
+    private String url;
     private String username;
     private char[] password;
     
     public UserRepo(String username, char[] password)
     {
-        //connection to db
+        this.url = "jdbc:postgres://localhost/gruppo8_se";
         this.username = username;
         this.password = password;
     }
     
-    public User queryCurrentUser()
+    public Connection connect(String username, char[] password) throws ClassNotFoundException, SQLException
+    {
+        //Open the connection to PostreSQL Database
+        Class.forName("org.postgresql.Driver");
+        connection = DriverManager.getConnection(url, username, String.valueOf(password));
+        JOptionPane.showMessageDialog(null,"Connection Established!");
+        return connection;
+    }        
+    
+    public User queryCurrentUser() throws SQLException
     {
         // Return a model of the current user
-        return null;
+        String query = "SELECT r1.rolname as userrole "
+                +       "FROM pg_catalog.pg_roles r "
+                +       "JOIN pg_catalog.pg_auth_members m " 
+                +       "ON (m.member = r.oid) " 
+                +       "JOIN pg_roles r1 ON (m.roleid = r1.oid) "
+                +       "WHERE r.rolcanlogin and current_user = '" + this.username + "'";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        Role role = null;
+        if (resultSet.getString("userrole").equals("SystemAdministrator"))
+            role = User.Role.SYSTEM_ADMINISTRATOR;
+        else if (resultSet.getString("userrole").equals("Planner")) 
+            role = User.Role.PLANNER;
+        User user = new User(role, this.username, this.password);
+        return user;
     }
     
-    public void closeConnection()
+    public void closeConnection() throws SQLException
     {
-        //close 
+        //Close the connection to the database
+        this.connection.close();
+        JOptionPane.showMessageDialog(null,"Connection Closed!");
     }        
     
 }
