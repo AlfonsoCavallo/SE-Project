c6a90f6f8bdb17240218609dc9821494a71f79b2
Giorgio <Giorgio@DESKTOP-6S1K87J>
2020-12-06 19:31:06 +0100

    Modified SelectMaintenanceActivityController

--- src/se/project/business_logic/controllers/activities_assignment/SelectMaintenanceActivityController.java
+++ src/se/project/business_logic/controllers/activities_assignment/SelectMaintenanceActivityController.java
@@ -1,7 +1,93 @@
 package se.project.business_logic.controllers.activities_assignment;
 
+import java.sql.SQLException;
+import se.project.business_logic.controllers.MainController;
+import se.project.business_logic.controllers.PlannerHomepageController;
+import se.project.presentation.views.activities_assignment.SelectMaintenanceActivityView;
+import static se.project.storage.DatabaseConnection.closeConnection;
+import se.project.storage.models.maintenance_activity.MaintenanceActivity;
+
 
 public class SelectMaintenanceActivityController
 {
+    private final SelectMaintenanceActivityView selectMaintenanceActivityView;
+    private MaintenanceActivity maintenanceActivity; 
     
+    /**
+     * 
+     * Creates a new ViewMaintenanceActivityController
+     */
+    public SelectMaintenanceActivityController()
+    {
+        this.selectMaintenanceActivityView = new SelectMaintenanceActivityView();
+        initListeners();
+    }
+    
+    /**
+     * 
+     *  Initializes the listeners of selectMaintenanceActivityView
+     */
+    public void initListeners()
+    {
+        selectMaintenanceActivityView.getjCloseConnectionLabel().addMouseListener(new java.awt.event.MouseAdapter()
+        {
+            public void mouseClicked(java.awt.event.MouseEvent evt)
+            {
+                try
+                {
+                    closeConnection();
+                }
+                catch (SQLException ex)
+                {
+                    System.err.println(ex.getMessage());
+                }
+                selectMaintenanceActivityView.dispose();
+                MainController.openLoginPage();
+            }
+        });
+
+        selectMaintenanceActivityView.getjGoBackLabel().addMouseListener(new java.awt.event.MouseAdapter()
+        {
+            public void mouseClicked(java.awt.event.MouseEvent evt)
+            {
+                goBackPlannerHomepage();
+                selectMaintenanceActivityView.dispose();
+            }  
+        });
+
+       selectMaintenanceActivityView.getjExitLabel().addMouseListener(new java.awt.event.MouseAdapter()
+       {
+           public void mouseClicked(java.awt.event.MouseEvent evt)
+            {
+                System.exit(0);
+            }
+       });
+       
+       selectMaintenanceActivityView.getjViewInfoPanel().addMouseListener(new java.awt.event.MouseAdapter()
+       {
+           public void mouseClicked(java.awt.event.MouseEvent evt)
+           {
+               openMaintenanceActivityInfoView();
+           }        
+       });
+        
+    }
+    
+    /**
+     * 
+     * Opens the planner homepage using its controller
+     */
+    public static void goBackPlannerHomepage()
+    {
+        new PlannerHomepageController();
+    }
+    
+    /**
+     * 
+     * Opens the maintenance activity info view using its controller
+     */
+    public void openMaintenanceActivityInfoView()
+    {
+        new MaintenanceActivityInfoController(this.maintenanceActivity);
+    }
 }
